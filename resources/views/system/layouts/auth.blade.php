089ca3d0ca3b6e438ccffca7b6b5e2bed4397705
jinomorales <jino0000morales0000@gmail.com>
2024-05-16 10:37:11 -0500

    otro

--- resources/views/system/layouts/auth.blade.php
+++ resources/views/system/layouts/auth.blade.php
@@ -13,12 +13,18 @@
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="{{ asset('porto-light/vendor/bootstrap/css/bootstrap.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('porto-light/vendor/bootstrap/css/bootstrap.css') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/vendor/animate/animate.css') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/vendor/font-awesome/css/fontawesome-all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/css/theme.css') }}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.26.29/sweetalert2.min.css" />
     <link rel="stylesheet" href="{{ asset('theme/admin_styles.css') }}" />
+     --}}
+     <link rel="stylesheet" href="{{ asset('theme/login/admin_styles.css') }}" />
+    <link rel="stylesheet" href="{{ asset('theme/login/admin_styles.css') }}" />
+    <link rel="stylesheet" href="{{ asset('theme/login/admin_styles.css') }}" />
+    <link rel="stylesheet" href="{{ asset('theme/login/admin_styles.css') }}" />
+    <link rel="stylesheet" href="{{ asset('theme/login/admin_styles.css') }}" />
 
     @if (file_exists(public_path('theme/custom_styles.css')))
         <link rel="stylesheet" href="{{ asset('theme/custom_styles.css') }}" />
